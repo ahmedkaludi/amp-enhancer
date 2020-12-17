edf9599510021e838c67b8ed5b991677fd7038b1
Syed Imran <41104393+syedimran22@users.noreply.github.com>
2020-12-17 19:14:24 +0530

    Added Popup Feature

---
+++ includes/features/popup/popup-frontend.php
@@ -0,0 +1,106 @@
+<?php
+if(!isset($_COOKIE['ampenhancer_popup'])){
+   add_action('wp_footer','amp_enhancer_popup_feature_output');
+}
+
+function amp_enhancer_popup_feature_output(){
+
+    $condition_logics = amp_enhancer_pop_up_post_type_data();
+       
+     if(!$condition_logics){
+         return ;
+     }
+    if($condition_logics){
+      foreach( $condition_logics as $condition_logic ) { 
+    
+           $post_id= $condition_logic['post_id'];
+           $display_condition = $condition_logic['display_condition'];
+
+           if(isset($display_condition) && $display_condition == 'homepage' && (!is_home() && 
+             !is_front_page())){
+              continue;
+           }
+           
+           $load_everytime = ( isset($condition_logic['cookie_type']) &&  $condition_logic['cookie_type'] == 'en_popup_without_cookie' ) ? 'data-persist-dismissal="false"' : '';
+
+           $dataPersistDismissalValue = (isset($condition_logic['en_popup_set_time'])  && $condition_logic['en_popup_set_time'] == 1 ) ? 'data-persist-dismissal="false"' : '';
+
+           //store cookie if true
+                $cookieFormOpen  = $cookieFormClose = '';
+                if( isset($condition_logic['cookie_type']) && isset($condition_logic['en_popup_set_time'])  &&   $condition_logic['en_popup_set_time'] == 1 && $condition_logic['cookie_type'] == 'en_popup_with_cookie'){
+
+                    $formUrl   = admin_url('admin-ajax.php?action=amp_enhancer_popup_dismiss_consent');
+                    $formUrl   = preg_replace('#^https?:#', '', $formUrl);
+                    $cookieFormOpen = '<form class="amp_pu_cf" action-xhr="'.esc_url($formUrl).'" method="post" target="_top"><input type="hidden" name="dismiss_id" value="'.$post_id.'"/>';
+                    $cookieFormClose = '</form>';
+                }  
+            
+           ?>
+           <amp-user-notification id="amp-en-pop-up-<?php esc_attr_e($post_id); ?>"   <?php echo $load_everytime; ?> layout="nodisplay" <?php echo $dataPersistDismissalValue; ?> style="background: rgba(240, 240, 240, 0.83); border-color: #242323b3; z-index: 10000; height: -webkit-fill-available; height: -moz-fill-available; top:0;"   >
+              <div class="en-afwp"> 
+                  <?php  
+                    $pop_up_data['data'] = '<p>' .$condition_logic['content']. '</p>';
+                    ?>
+                 <div class ="en-apf post-<?php esc_attr_e($post_id); ?>"   style="  position: relative;background:#fff;box-shadow: 0px 0px 60px 0px #d0d0d0;border-radius: 10px;text-align: center;padding: 40px;overflow-y: auto;max-height: 90vh;"> 
+                      <?php
+
+                        echo do_shortcode($pop_up_data['data']);
+
+                      echo $cookieFormOpen;?>
+                      <button on="tap:amp-en-pop-up-<?php esc_attr_e($post_id); ?>.dismiss"
+                      class="en-apcb"><?php echo 'X';?>
+                      </button> 
+                    <?php echo $cookieFormClose;?>
+                </div>  
+            </div>
+         </amp-user-notification>
+      <?php
+      } //for each closes
+   
+   }
+}
+
+
+
+//FrontEnd
+ 
+function amp_enhancer_pop_up_post_type_data($contentArray=''){
+
+        $post_idArray = array();
+        $arg = array(
+              'post_type' => 'ampenhancerpopup',
+              'post_status' => 'publish',
+              'posts_per_page' => -1,
+          );
+        if($contentArray != ''){
+          $arg['post__in'] = array($contentArray['popup_btn_link']);
+        }
+
+        $query = new WP_Query($arg);
+        while ($query->have_posts()) {
+              $query->the_post();
+              $post_idArray[] = get_the_ID();
+          } 
+          wp_reset_query();
+          wp_reset_postdata();
+    if(count($post_idArray)>0){    
+
+      $returnData = array();
+      foreach ($post_idArray as $key => $post_id){ 
+         
+              $returnData[] = array(
+                    'content' => get_post_field('en_popup_content', $post_id),
+                    'display_condition' => get_post_meta( $post_id, 'en_popup_display', true),
+                    'cookie_type'  => get_post_meta( $post_id, 'en_popup_cookie_type', true), 
+                    'en_popup_set_time' => get_post_meta( $post_id, 'en_popup_set_time', true),
+                    'en_cookie_duration' => get_post_meta( $post_id, 'en_cookie_time', true),
+                    'post_id'   => $post_id,
+
+                  ); 
+            }
+     return $returnData;   
+  }//iF Closed post_idArray     
+   return false;
+}  
+
+
