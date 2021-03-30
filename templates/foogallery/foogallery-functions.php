ef02869be444cbc9f45da062a7ac9a32d6b6a7d1
Syed Imran <41104393+syedimran22@users.noreply.github.com>
2021-03-30 13:52:16 +0100

    Fixed the error of failed to open gallery-image-viewer.php file of foogallery plugin

--- templates/foogallery/foogallery-functions.php
+++ templates/foogallery/foogallery-functions.php
@@ -12,8 +12,10 @@ function amp_enhancer_foo_gal_reader_css(){ ?>
 function amp_enhancer_foogallery_load_gallery_template($boolean, $gallery, $template_location){
 
     if(strpos($template_location, 'image-viewer/gallery-image-viewer.php')){
-      	$amp_template = AMP_ENHANCER_TEMPLATE_DIR.'foogallery\default-templates\gallery-image-viewer.php';
-      	load_template( $amp_template, false );
+      	$amp_template = AMP_ENHANCER_TEMPLATE_DIR.'foogallery/default-templates/gallery-image-viewer.php';
+      	if(file_exists($amp_template)){
+      	  load_template( $amp_template, false );
+        }
      	return true;
     }else{
     	return false;
