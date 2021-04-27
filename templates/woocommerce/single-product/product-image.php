24bbfb8ccda62a2cbf55b7175343fdf22d4f2948
Syed Imran <41104393+syedimran22@users.noreply.github.com>
2021-04-27 12:49:39 +0530

    #42 Resolved the issue product images not being cropped

--- templates/woocommerce/single-product/product-image.php
+++ templates/woocommerce/single-product/product-image.php
@@ -75,10 +75,42 @@ $wrapper_classes   = apply_filters(
 				 if($i == 1){
 				    $checked = 'checked="checked"';
 				 }
+
+					$flexslider        = (bool) apply_filters( 'woocommerce_single_product_flexslider_enabled', get_theme_support( 'wc-product-gallery-slider' ) );
+					$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
+					$thumbnail_size    = apply_filters( 'woocommerce_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
+					$image_size        = apply_filters( 'woocommerce_gallery_image_size', $flexslider || $main_image ? 'woocommerce_single' : $thumbnail_size );
+					$full_size         = apply_filters( 'woocommerce_gallery_full_size', apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' ) );
+					$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, $thumbnail_size );
+					$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
+					$alt_text          = trim( wp_strip_all_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) );
+					$image             = wp_get_attachment_image(
+						$attachment_id,
+						$image_size,
+						false,
+						apply_filters(
+							'woocommerce_gallery_image_html_attachment_image_params',
+							array(
+								'title'                   => _wp_specialchars( get_post_field( 'post_title', $attachment_id ), ENT_QUOTES, 'UTF-8', true ),
+								'data-caption'            => _wp_specialchars( get_post_field( 'post_excerpt', $attachment_id ), ENT_QUOTES, 'UTF-8', true ),
+								'data-src'                => esc_url( $full_src[0] ),
+								'data-large_image'        => esc_url( $full_src[0] ),
+								'data-large_image_width'  => esc_attr( $full_src[1] ),
+								'data-large_image_height' => esc_attr( $full_src[2] ),
+								'class'                   => esc_attr( $main_image ? 'wp-post-image' : '' ),
+								'lightbox'  =>'',
+							),
+							$attachment_id,
+							$image_size,
+							$main_image
+						)
+					);
 				 ?>
 			       <input class="carousel-open" type="radio" id="carousel-<?php echo esc_attr($i); ?>" name="carousel" aria-hidden="true" hidden="" <?php echo esc_attr($checked); ?> >
 					<div class="carousel-item ">
-						<img lightbox src="<?php echo esc_url($full_src[0]); ?>">
+
+						<?php  echo $image;  ?>
+
 					</div>
 					
 					
