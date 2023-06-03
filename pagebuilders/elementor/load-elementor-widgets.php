a68a6c093c6733f79d22a615d0aa47adc99c5938
Sanjeev Kumar <sanjeevsetu@gmail.com>
2023-06-03 14:31:26 +0530

    #73

--- pagebuilders/elementor/load-elementor-widgets.php
+++ pagebuilders/elementor/load-elementor-widgets.php
@@ -24,7 +24,7 @@ class Amp_Enhancer_Elementor_Widgets_Loading {
 	     require_once( AMP_ENHANCER_PLUGIN_DIR . 'pagebuilders/elementor/widgets/amp-video.php' );
 	 }
 
-	public function register_widgets($widgets_manager) {
+	public function register_widgets() {
 		// Register Widgets
 		if ( (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint()) ) {
 
@@ -41,6 +41,23 @@ class Amp_Enhancer_Elementor_Widgets_Loading {
 		}
 	}
 
+	public function register() {
+		// Register Widgets
+		if ( (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint()) ) {
+
+			$this->include_widgets_files();
+
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\Amp_Accordion() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\Amp_Toggle() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\Amp_Tabs() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\AMP_Alert() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\AMP_Counter() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\AMP_Progress() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\Amp_Image_Carousel() );
+	        \Elementor\Plugin::instance()->widgets_manager->register( new Widgets\Amp_Video() );
+		}
+	}
+
 
 	public function amp_enhancer_elementor_add_amp_script_wrapper($content){
 		$post_id = get_the_ID();
@@ -49,14 +66,27 @@ class Amp_Enhancer_Elementor_Widgets_Loading {
 	   		 $popup_enabled = \ElementorPro\Modules\ThemeBuilder\Module::instance()->get_conditions_manager()->get_documents_for_location( "popup" );
 	     }
 
-	   if ( (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() && class_exists('\Elementor\Plugin') && (\Elementor\Plugin::$instance->db->is_built_with_elementor($post_id) || (isset($popup_enabled) && !empty($popup_enabled)))   ) ) {
-	   	
-		  	$script_url = str_replace('http:','https:',AMP_ENHANCER_PAGEBUILDER_URI).'elementor/amp-script/amp-enhancer-elementor.js?ver='.AMP_ENHANCER_VERSION;
-		 	$amp_script = ' <amp-script src="'.esc_url_raw($script_url).'" sandbox="allow-forms" >';
-		 	$close_script = '</amp-script>';
 
-		  $content =  $amp_script.$content.$close_script;
-	    }
+		if(defined('ELEMENTOR_VERSION') && version_compare(ELEMENTOR_VERSION, '3.2.0') >= 0 ) {
+			
+			if ( (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() && class_exists('\Elementor\Plugin') && (\Elementor\Plugin::$instance->documents->get( $post_id )->is_built_with_elementor() || (isset($popup_enabled) && !empty($popup_enabled)))   ) ) {
+	   	
+				$script_url = str_replace('http:','https:',AMP_ENHANCER_PAGEBUILDER_URI).'elementor/amp-script/amp-enhancer-elementor.js?ver='.AMP_ENHANCER_VERSION;
+			    $amp_script = ' <amp-script src="'.esc_url_raw($script_url).'" sandbox="allow-forms" >';
+			    $close_script = '</amp-script>';  
+				$content =  $amp_script.$content.$close_script;
+		  	}
+
+		}else{
+
+			if ( (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() && class_exists('\Elementor\Plugin') && (\Elementor\Plugin::$instance->db->is_built_with_elementor($post_id) || (isset($popup_enabled) && !empty($popup_enabled)))   ) ) {
+	   	
+				$script_url = str_replace('http:','https:',AMP_ENHANCER_PAGEBUILDER_URI).'elementor/amp-script/amp-enhancer-elementor.js?ver='.AMP_ENHANCER_VERSION;
+			    $amp_script = ' <amp-script src="'.esc_url_raw($script_url).'" sandbox="allow-forms" >';
+			    $close_script = '</amp-script>';  
+				$content =  $amp_script.$content.$close_script;
+		  	}
+		}	   
       
        return $content;
 	}
@@ -130,10 +160,15 @@ class Amp_Enhancer_Elementor_Widgets_Loading {
 
 	public function __construct() {
 		
-		// Register widgets		
-		//add_action( 'elementor/elements/elements_registered', [ $this, 'register_elements' ], 999999);
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ], 999999 );
+		// Register widgets						
 
+		if(defined('ELEMENTOR_VERSION') && version_compare(ELEMENTOR_VERSION, '3.5.0') >= 0 ) {
+			add_action( 'elementor/widgets/register', [ $this, 'register' ], 999999 );
+		}
+		else{
+			add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ], 999999 );
+		}
+		
 		add_filter('elementor/frontend/the_content',[ $this, 'amp_enhancer_elementor_add_amp_script_wrapper' ],999,1);
 
 		add_filter('amp_enhancer_content_html_last_filter',[ $this, 'amp_enhancer_elementor_postid_custom_css' ],999,1);
